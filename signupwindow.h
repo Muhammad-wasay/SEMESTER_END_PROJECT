1a2498f58287f91c8f1eea160bd4c0d1aec02ee1
Muhammad-wasay <wasayrafiq6@gmail.com>
2025-05-04 02:51:33 +0500

    Add files via upload

---
+++ signupwindow.h
@@ -0,0 +1,34 @@
+#ifndef SIGNUPWINDOW_H
+#define SIGNUPWINDOW_H
+#include <centralwindow.h>
+class MainWindowofBANK;
+
+
+#include <QMainWindow>
+
+namespace Ui {
+class signupwindow;
+}
+
+class signupwindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit signupwindow(QWidget *parent = nullptr);
+    ~signupwindow();
+
+private slots:
+
+    void on_backbtn_clicked();
+    void on_createbtn_clicked();
+
+private:
+    Ui::signupwindow *ui;
+    Centralwindow *central;
+    MainWindowofBANK *mwb;
+    //new line code
+     QString generateAccountNumber(const QString &bankPrefix = "IB");
+};
+
+#endif // SIGNUPWINDOW_H
